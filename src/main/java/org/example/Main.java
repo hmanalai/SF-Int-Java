03918028bc62527de9e583a493e894537ae8c0dc
Simon <simon@dancingcloudservices.com>
2023-07-26 08:09:03 -0600

    Initial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    Object x = "Hello Java 20 preview world!";
+    System.out.println(
+      switch (x) {
+          case String s when (s.length() > 4) -> "Message is " + s;
+          default -> "Message is unknown";
+      }
+    );
+  }
 }
