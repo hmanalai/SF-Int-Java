290ac896d9ca194d47de6c9d9171f5d8a23574b1
Simon <simon@dancingcloudservices.com>
2023-07-26 13:09:03 -0600

    Day 1 lunch

---
+++ src/main/java/student/Student.java
@@ -0,0 +1,60 @@
+package student;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Student {
+  // let's make this immutable... (using final fields is likely smart anyway)
+  private /*final*/ String name;
+  private double gpa;
+  // courses... array? fixed length, maybe not. List? allows duplicates :(
+  // go with Set, to prevent duplicates
+  private Set<String> courses;
+
+  // mutable objects provided by a caller are not safe for us to use!!!
+  public Student(String name, double gpa, Set<String> courses) {
+    this.name = name;
+    this.gpa = gpa;
+//    this.courses = courses; // really bad, see comment above
+//    this.courses = new HashSet<>(courses); // our own copy, but still mutable :)
+
+    //does this work?? Not sure, but ugly anyway
+    String[] theCourses = (String[])courses.toArray();
+    // Set<StringBuilder> still permits changes to the individual StringBuilders!!!
+    this.courses = Set.of(theCourses); // unmodifiable Set :) since Java 9
+
+    // For List prior to Java 9, look at Arrays.asList
+    // -- not really immutable!!! Can call set on this!!! (but not add/remove/clear)
+
+    // This is a "proxy"
+    // still vulnerable to caller provided structure being changed
+//    this.courses = Collections.unmodifiableSet(courses);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getGpa() {
+    return gpa;
+  }
+
+  // handing a mutable reference to our internal data is unsafe!
+//  someStudent.getCourses().add("Underwater basket weaving")
+  public Set<String> getCourses() {
+    return courses; // OK if our courses is immutable, e.g. Set.of..
+    // wrap for the caller, but allow ourselves to change it?
+//    return Collections.unmodifiableSet(courses);
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+      "name='" + name + '\'' +
+      ", gpa=" + gpa +
+      ", courses=" + courses +
+      '}';
+  }
+}
